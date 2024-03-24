ca00e179e825f7f3f58897b6127d0dd91c94f51c
MujtabaMHaq <mujtaba317364@gmail.com>
2024-03-24 14:21:07 +0500

    Arrays - Loops Assignment

---
+++ Arrays/app.js
@@ -0,0 +1,82 @@
+// Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+// var  allStd=[];
+//     allStd[0] = prompt("Enter your Name : ");
+//     allStd[1] = prompt("Enter your Name : ");
+//     allStd[2] = prompt("Enter your Name : ");
+// var allStdMarks=[];
+//  allStdMarks[0] = prompt("Enter your Score : ");
+//  allStdMarks[1] = prompt("Enter your Score : ");
+//  allStdMarks[2] = prompt("Enter your Score : ");
+// console.log("Score of " + allStd[0] + " is " + allStdMarks[0] + ".Percentage: " + (allStdMarks[0]/500)*100 + "%" )
+// console.log("Score of " + allStd[1] + " is " + allStdMarks[1] + ".Percentage: " + (allStdMarks[1]/500)*100 + "%")
+// console.log("Score of " + allStd[2] + " is " + allStdMarks[2] + ".Percentage: " + (allStdMarks[2]/500)*100 + "%")
+
+// 9. Initialize an array with color names. Display the array
+// elements in your browser.
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end
+// & add that color to the end of the array. Display the
+// updated array in your browser.
+// c. Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then
+// Arrays | JAVASCRIPT
+// Page 3 of 6
+// remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+
+
+// var colors=[];
+
+// colors.unshift(prompt("Add Your Color to the Beginning"))
+// console.log(colors);
+
+
+// colors.push(prompt("Add Your Color to End"))
+// console.log(colors);
+
+// colors.unshift(prompt("Add more colors to Begining"))
+// colors.unshift(prompt("Add more colors to Begining"))
+// console.log(colors);
+
+// colors.shift(0 , 1)
+// console.log(colors)
+
+// colors.pop()
+// console.log(colors)
+
+// colors.splice(prompt("Enter Desired Index") , 0 , prompt("Enter Your Desired Color"))
+// console.log(colors)
+
+// colors.splice(prompt("Enter Desired Index") , prompt("Enter Your Delete Count"))
+// console.log(colors)
+
+// var stdScores=[]
+
+// stdScores[0]=(+prompt("Enter Your Score(s)"));
+// stdScores[1]=(+prompt("Enter Your Score(s)"));
+// stdScores[2]=(+prompt("Enter Your Score(s)"));
+// stdScores[3]=(+prompt("Enter Your Score(s)"));
+// stdScores[4]=(+prompt("Enter Your Score(s)"));
+// console.log(stdScores);
+
+// stdScores.sort();
+// console.log(stdScores);
+// stdScores.reverse();
+// console.log(stdScores);
+
---
+++ JavaScript/app.js
@@ -0,0 +1,35 @@
+// chapter 1: Alerts
+alert("Welcome to my website.");
+
+// chapter 2: Strig variables
+var firstName = "Mujtaba";
+
+// chapter 3 numeric var
+var age = 25;
+
+// chapter 5 math operators
+console.log("Your name is " + firstName + " and your age is " + age );
+
+// chapter 6 multiply operator
+var num1 = 2;
+var num2 = 3;
+var add=num1*num2;
+
+console.log("the result of num1 and num2 if multiplied is " + add );
+
+var a = 1;
+var a = "1";
+
+//  chapter 9 prompt
+
+prompt("This is a promt", "placeholder");
+
+// chapter 10 if statement
+
+var mySalary = prompt("Please enter your age:");
+
+if( mySalary > 18 ){
+    console.log("Welcome to my website.");
+}else{
+    console.log("You're age should be over 18.");
+}
